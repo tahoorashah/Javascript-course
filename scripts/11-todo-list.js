9ff8daf34f53ce9fcdd1ff67fa324d07b8191107
Tahoora <shahtahoora250@gmail.com>
2025-05-06 10:03:42 +0530

    update

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,3 +1,4 @@
+//1st proj---
 const todoList = [];
 
 function addTodo() { 
@@ -10,4 +11,37 @@ function addTodo() {
 
  //to empty the placeholder after clicking add
  inputElement.value = ''
+}
+
+
+//2nd proj---
+//use of for-loop  
+const todolist = ['make dinner', 'wash dishes'];
+
+//accumulator pattern
+let todolistHTML = '';  
+
+for (let i = 0; i < todolist.length; i++){
+  const todo = todolist[i]; //variable stores string
+  const html = `<p>${todo}</p>`;
+
+  //combining all the code
+  todolistHTML += html; 
+} 
+console.log(todolistHTML);
+
+//prints todolist on webpage
+document.querySelector('.js-todo-list')
+.innerHTML = todolistHTML; 
+
+function renderTodolist() { 
+ const inputelement = document.querySelector('.js-name-input');
+ const name = inputelement.value;
+ 
+
+ todolist.push(name);
+ console.log(todolist);
+
+ //to empty the placeholder after clicking add
+ inputelement.value = ''
 }
