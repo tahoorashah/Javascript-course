01ad6df22f196dc3a36fca5daa5df814e386f6fd
Tahoora <shahtahoora250@gmail.com>
2025-05-08 16:25:27 +0530

    update

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -15,12 +15,20 @@ function addTodo() {
 
 
 //2nd proj---
-//use of for-loop  
+//use of for-loop 
+//save the data step-- 
 const todolist = ['make dinner', 'wash dishes'];
+//calling function rederTodolist()
+renderTodolist();
 
+//function that renders todo each time we add it--
+function renderTodolist(){
+
+  //steps to generate HTML
 //accumulator pattern
 let todolistHTML = '';  
 
+//steps inside loop called 'generating the HTML'
 for (let i = 0; i < todolist.length; i++){
   const todo = todolist[i]; //variable stores string
   const html = `<p>${todo}</p>`;
@@ -33,8 +41,9 @@ console.log(todolistHTML);
 //prints todolist on webpage
 document.querySelector('.js-todo-list')
 .innerHTML = todolistHTML; 
+}
 
-function renderTodolist() { 
+function Todolist() { 
  const inputelement = document.querySelector('.js-name-input');
  const name = inputelement.value;
  
@@ -43,5 +52,8 @@ function renderTodolist() {
  console.log(todolist);
 
  //to empty the placeholder after clicking add
- inputelement.value = ''
+ inputelement.value = '';
+
+ //calling function renderTodolist
+ renderTodolist();
 }
