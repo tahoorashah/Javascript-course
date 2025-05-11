054ce8a9300ed9e6c734cae491ff39071c7094d4
Tahoora <shahtahoora250@gmail.com>
2025-05-11 19:17:23 +0530

    update

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -57,3 +57,71 @@ function Todolist() {
  //calling function renderTodolist
  renderTodolist();
 }
+
+
+//3rd proj
+//use of for-loop 
+//save the data step-- 
+const todolistTwo = [{
+  name:'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name: 'wash dishes',
+  dueDate: '2022-12-22'
+}];
+//calling function rederTodolistTwo()
+renderTodolistTwo();
+
+//function that renders todo each time we add it--
+function renderTodolistTwo(){
+
+  //steps to generate HTML
+//accumulator pattern
+let todolistTwoHTML = '';  
+
+//steps inside loop called 'generating the HTML'
+for (let i = 0; i < todolistTwo.length; i++) {
+  const todoObject = todolistTwo[i]; //variable stores string
+  //const name = todoObject.name;
+  //const dueDate = todoObject.dueDate;
+  const { name, dueDate } = todoObject;
+  const html = `
+    <p>
+  ${name} ${dueDate }
+   <button onclick="
+     todolistTwo.splice(${i}, 1);
+     renderTodolistTwo();
+  " > Delete</button>
+  </p>
+  `;
+
+  //combining all the code
+  todolistTwoHTML += html; 
+} 
+console.log(todolistTwoHTML);
+
+//prints todolist on webpage
+document.querySelector('.js-todo-listTwo')
+.innerHTML = todolistTwoHTML; 
+}
+
+function addTodoTwo() { 
+
+  const inputelement = document.querySelector('.js-nametwo-input');
+  const inputDateElement = document.querySelector('.js-due-date-input');
+
+  const name = inputelement.value.trim();
+  const dueDate = inputDateElement.value;
+
+
+  todolistTwo.push({
+    name,
+    dueDate
+  });
+  console.log(todolistTwo);
+
+  inputelement.value = '';
+  inputDateElement.value = '';
+
+  renderTodolistTwo();
+}
